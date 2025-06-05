5f26d1dd41b4d7bf908ef1abc4cbe60effb58a2c
Desarrollo <Desarrollo@192.168.0.105>
2025-06-05 19:58:47 +0200

    Colecciones

---
+++ bases/src/com/ipartek/formacion/ejemplos/Colecciones.java
@@ -0,0 +1,30 @@
+package com.ipartek.formacion.ejemplos;
+
+import java.util.ArrayList;
+
+public class Colecciones {
+	public static void main(String[] args) {
+		ArrayList<String> al = new ArrayList<String>();
+		
+		al.add("Uno");
+		al.add("lasdjflaksdjf");
+		al.add("Dso");
+		al.add("Cuatro");
+		
+		System.out.println(al);
+		
+		al.remove(1);
+		
+		al.set(1, "Dos");
+		
+		al.add(2, "Tres");
+		
+//		al.set(1, null);
+		
+		System.out.println(al);
+		
+		System.out.println(al.get(3));
+		
+		System.out.println(al.size());
+	}
+}
