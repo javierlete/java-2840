42717dc0fbcd205b6efa6de917b391cd98d0292a
JavierLete <JavierLete@ASUS-AMD>
2025-07-09 17:49:36 +0200

    Aplicación terminada con DAO real y base de datos

--- ejemploweb/src/main/java/com/ipartek/formacion/tienda/accesodatos/DaoProductoSqlite.java
+++ ejemploweb/src/main/java/com/ipartek/formacion/tienda/accesodatos/DaoProductoSqlite.java
@@ -1,38 +1,59 @@
 package com.ipartek.formacion.tienda.accesodatos;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.ipartek.formacion.bibliotecas.DaoException;
 import com.ipartek.formacion.bibliotecas.DaoJdbc;
-import com.ipartek.formacion.tienda.mocks.TiendaMock;
 import com.ipartek.formacion.tienda.modelos.Producto;
 
 public class DaoProductoSqlite extends DaoJdbc<Producto> implements DaoProducto {
 
 	public DaoProductoSqlite(String url) {
 		super(url);
+		
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e) {
+			throw new DaoException("No se ha podido encontrar el driver de SQLite", e);
+		}
 	}
 
 	@Override
 	public Iterable<Producto> obtenerTodos() {
-		return TiendaMock.PRODUCTOS.values();
+		return ejecutarConsultaSql("SELECT * FROM productos", rs -> filaAProducto(rs));
 	}
 
 	@Override
 	public Producto obtenerPorId(Long id) {
-		return TiendaMock.PRODUCTOS.get(id);
+		return ejecutarConsultaSqlUno("SELECT * FROM productos WHERE id=?", rs -> filaAProducto(rs), id);
 	}
 
 	@Override
 	public void insertar(Producto producto) {
-		System.out.println("INSERTAR: " + producto);
+		ejecutarConsultaSql("INSERT INTO productos (nombre, precio) VALUES (?,?)", producto.getNombre(), producto.getPrecio());
 	}
 
 	@Override
 	public void modificar(Producto producto) {
-		System.out.println("MODIFICAR: " + producto);
+		ejecutarConsultaSql("UPDATE productos SET nombre=?, precio=? WHERE id=?", producto.getNombre(), producto.getPrecio(), producto.getId());
 	}
 
 	@Override
 	public void borrar(Long id) {
-		System.out.println("BORRAR: " + id);
+		ejecutarConsultaSql("DELETE FROM productos WHERE id=?", id);
+	}
+
+	private Producto filaAProducto(ResultSet rs) {
+		try {
+			var id = rs.getLong("id");
+			var nombre = rs.getString("nombre");
+			var precio = rs.getBigDecimal("precio");
+			
+			return new Producto(id, nombre, precio);
+		} catch (SQLException e) {
+			throw new DaoException("Fallo al mapear", e);
+		}
 	}
 
 }
