bdddfdd27924d1a245aa2b21cd79365fc1a81d2c
JavierLete <JavierLete@ASUS-AMD>
2025-07-14 16:14:02 +0200

    Autorización de acceso administrador con filtros

---
+++ ejemploweb/src/main/java/com/ipartek/formacion/tienda/filtros/AdminFilter.java
@@ -0,0 +1,27 @@
+package com.ipartek.formacion.tienda.filtros;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebFilter("/admin/*")
+public class AdminFilter extends HttpFilter {
+
+	private static final long serialVersionUID = 6888202794692302818L;
+
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		if(request.getSession().getAttribute("email") == null) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return;
+		}
+		
+		super.doFilter(request, response, chain);
+	}
+}
