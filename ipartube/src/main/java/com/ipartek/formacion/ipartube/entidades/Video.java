c10bfbc367cbba99078ab1e0173792d4b25d6349
JavierLete <JavierLete@host.docker.internal>
2025-09-01 17:02:01 +0200

    Entidades

---
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/entidades/Video.java
@@ -0,0 +1,39 @@
+package com.ipartek.formacion.ipartube.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+@Entity
+@Table(name = "videos")
+public class Video {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank
+	@Size(max = 100)
+	private String titulo;
+	
+	@Lob
+	@Size(max = 2000)
+	private String descripcion;
+	
+	@NotBlank
+	@Size(max = 255)
+	private String url;
+}
