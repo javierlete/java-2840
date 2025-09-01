48d75a8a7e673cd637f1c551f1628c8fae0b4d1d
JavierLete <JavierLete@host.docker.internal>
2025-09-01 17:03:42 +0200

    Servicios

---
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/servicios/AnonimoService.java
@@ -0,0 +1,8 @@
+package com.ipartek.formacion.ipartube.servicios;
+
+import com.ipartek.formacion.ipartube.entidades.Video;
+
+public interface AnonimoService {
+	Iterable<Video> verListadoVideos();
+	Video verDetalleVideo(Long id);
+}
