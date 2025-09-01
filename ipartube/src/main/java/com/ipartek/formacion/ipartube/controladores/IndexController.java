d8a087dd2836d6d6515ad4842c5c49e0f5c9e503
JavierLete <JavierLete@host.docker.internal>
2025-09-01 17:24:02 +0200

    Capa presentación con controlador y vistas usando Thymeleaf y DevTools

--- ipartube/src/main/java/com/ipartek/formacion/ipartube/controladores/IndexController.java
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/controladores/IndexController.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ipartek.formacion.ipartube.entidades.Video;
 import com.ipartek.formacion.ipartube.servicios.AnonimoService;
 
 @Controller
@@ -17,13 +15,16 @@ public class IndexController {
 	private AnonimoService anonimoService;
 	
 	@GetMapping
-	@ResponseBody
-	public Iterable<Video> index(Model modelo) {
+	public String index(Model modelo) {
 		var videos = anonimoService.verListadoVideos();
-		
-		return videos;
-		
-//		modelo.addAttribute("videos", videos);
-//		return "index";
+		modelo.addAttribute("videos", videos);
+		return "index";
+	}
+	
+	@GetMapping("/video")
+	public String video(Long id, Model modelo) {
+		var video = anonimoService.verDetalleVideo(id);
+		modelo.addAttribute("video", video);
+		return "video";
 	}
 }
