6ac910c9e52e2f96b163b146132ead2170918137
JavierLete <JavierLete@host.docker.internal>
2025-09-03 17:25:45 +0200

    Paginación y ordenación en Frontend

--- ipartube/src/main/java/com/ipartek/formacion/ipartube/rest/VideoRestController.java
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/rest/VideoRestController.java
@@ -1,9 +1,14 @@
 package com.ipartek.formacion.ipartube.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ipartek.formacion.ipartube.entidades.Video;
@@ -14,12 +19,19 @@ import com.ipartek.formacion.ipartube.servicios.AnonimoService;
 public class VideoRestController {
 	@Autowired
 	private AnonimoService anonimoService;
-	
+
 	@GetMapping
 	public Iterable<Video> get() {
 		return anonimoService.verListadoVideos();
 	}
-	
+
+	@GetMapping("paginados")
+	public Page<Video> getPaginados(@RequestParam(defaultValue = "0") int pagina,
+			@RequestParam(defaultValue = "10") int tamano, @RequestParam(defaultValue = "ASC") String ordenacion) {
+		return anonimoService
+				.verListadoVideos(PageRequest.of(pagina, tamano, Sort.by(Direction.fromString(ordenacion), "titulo")));
+	}
+
 	@GetMapping("{id}")
 	public Video getVideo(@PathVariable Long id) {
 		return anonimoService.verDetalleVideo(id);
--- ipartube/src/main/resources/static/js/frontend.js
+++ ipartube/src/main/resources/static/js/frontend.js
@@ -1,33 +1,80 @@
 'use strict';
 
 let ul, iframe;
+let primera, anterior, siguiente, ultima;
+let numero, tamanoPagina, paginas, registros;
 
 window.addEventListener('DOMContentLoaded', async () => {
-	
 	ul = document.querySelector('ul');
 	iframe = document.querySelector('iframe');
-		
+
+	primera = document.getElementById('primera');
+	anterior = document.getElementById('anterior');
+	siguiente = document.getElementById('siguiente');
+	ultima = document.getElementById('ultima');
+
+	numero = document.getElementById('numero');
+	tamanoPagina = document.getElementById('tamano');
+	paginas = document.getElementById('paginas');
+	registros = document.getElementById('registros');
+
 	principal();
 });
 
 async function detalle(id) {
 	ul.style.display = 'none';
 	iframe.style.display = null;
-	
+
 	const respuesta = await fetch('/api/videos/' + id);
 	const video = await respuesta.json();
-	
+
 	iframe.src = video.url;
 }
 
-async function principal() {
+async function principal(pagina = 0, tamano = 2, ordenacion = 'ASC') {
 	iframe.style.display = 'none';
 	ul.style.display = null;
 
 	ul.innerHTML = '';
-		
-	const respuesta = await fetch('/api/videos');
-	const videos = await respuesta.json();
+
+	const respuesta = await fetch(`/api/videos/paginados?tamano=${tamano}&pagina=${pagina}&ordenacion=${ordenacion}`); // '/api/videos');
+	const contexto = await respuesta.json();
+
+	primera.onclick = e => {
+		e.preventDefault();
+
+		principal(0, tamano, ordenacion);
+	};
+
+	anterior.onclick = e => {
+		e.preventDefault();
+
+		principal(pagina - 1, tamano, ordenacion);
+	};
+
+	siguiente.onclick = e => {
+		e.preventDefault();
+
+		principal(pagina + 1, tamano, ordenacion);
+	};
+
+	ultima.onclick = e => {
+		e.preventDefault();
+
+		principal(contexto.totalPages - 1, tamano, ordenacion);
+	};
+
+	primera.style.display = contexto.first ? 'none' : null;
+	anterior.style.display = pagina == 0 ? 'none' : null;
+	siguiente.style.display = pagina == contexto.totalPages - 1 ? 'none' : null;
+	ultima.style.display = contexto.last ? 'none' : null;
+
+	numero.innerText = contexto.number;
+	tamanoPagina.innerText = contexto.size;
+	paginas.innerText = contexto.totalPages;
+	registros.innerText = contexto.totalElements;
+
+	const videos = contexto.content;
 
 	videos.forEach(video => {
 		const li = document.createElement('li');
@@ -37,7 +84,7 @@ async function principal() {
 				<a href="javascript:detalle(${video.id})">${video.titulo}</a>
 			</li>
 		`;
-		
+
 		ul.appendChild(li);
 	});
 }
