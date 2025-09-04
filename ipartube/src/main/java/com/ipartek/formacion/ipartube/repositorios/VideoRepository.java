1428dff1729401a5b483d700aa0409ac2dbe3fef
JavierLete <JavierLete@host.docker.internal>
2025-09-04 16:15:32 +0200

    Pasamos de JpaRepository a CrudRepository y PagingAndSortingRepository

--- ipartube/src/main/java/com/ipartek/formacion/ipartube/repositorios/VideoRepository.java
+++ ipartube/src/main/java/com/ipartek/formacion/ipartube/repositorios/VideoRepository.java
@@ -1,9 +1,10 @@
 package com.ipartek.formacion.ipartube.repositorios;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.ipartek.formacion.ipartube.entidades.Video;
 
-public interface VideoRepository extends JpaRepository<Video, Long> {
+public interface VideoRepository extends CrudRepository<Video, Long>, PagingAndSortingRepository<Video, Long> {
 	Iterable<Video> findByTituloContains(String tituloParcial);
 }
