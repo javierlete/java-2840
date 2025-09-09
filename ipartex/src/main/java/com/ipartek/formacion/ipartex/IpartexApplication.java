46659fc1568407d6311792ccb03d65e08e7d1d1e
Desarrollo <Desarrollo@192.168.0.100>
2025-09-09 16:14:32 +0200

    ipartex: Lombok, Validation, JPA, Spring Web, DevTools, Thymeleaf

---
+++ ipartex/src/main/java/com/ipartek/formacion/ipartex/IpartexApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ipartex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartexApplication.class, args);
+	}
+
+}
---
+++ ipartex/src/test/java/com/ipartek/formacion/ipartex/IpartexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartek.formacion.ipartex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
